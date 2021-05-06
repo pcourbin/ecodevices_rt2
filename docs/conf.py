abe3ae8ea830afc68541992cc712455312b8daa5
Pierre COURBIN <pierre.courbin@gmail.com>
2021-05-06 21:08:15 +0200

    Add action to publish docs

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
     "sphinx.ext.autosectionlabel",
+    "sphinx.ext.githubpages",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
